4e571e03709157b3d6ce94245bd70be01f2b099a
anushaarao3 <132684467+anushaarao3@users.noreply.github.com>
2026-06-10 22:18:47 +0530

    Update solution.py

--- 3_longest_substring_without_repeating_characters/anusha/solution.py
+++ 3_longest_substring_without_repeating_characters/anusha/solution.py
@@ -1,1 +1,17 @@
 # put your solution here
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        window = ""
+        max_len = 0
+        
+        for char in s:
+            if char in window:
+                dupind = window.index(char)
+                window = window[dupind + 1:]
+            window += char
+            max_len = max(max_len, len(window)) 
+        return max_len
