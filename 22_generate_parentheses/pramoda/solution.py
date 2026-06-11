052543ea997a94beb81e872cf46fe1127c320f4f
Pramoda S R <pramoda9.2.2004@gmail.com>
2026-06-11 19:07:55 +0530

    Implement generateParenthesis function using backtracking

---
+++ 22_generate_parentheses/pramoda/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(open_count: int, closed_count: int, path: list):
+            if open_count == closed_count == n:
+                res.append("".join(path))
+                return
+            if open_count < n:
+                path.append('(')
+                backtrack(open_count + 1, closed_count, path)
+                path.pop()
+            if closed_count < open_count:
+                path.append(')')
+                backtrack(open_count, closed_count + 1, path)
+                path.pop()
+
+        backtrack(0, 0, [])
+        return res
+
+s = Solution()
+print(s.generateParenthesis(8))
