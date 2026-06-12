feb52cd7a78d4b14805420efc2c73d31e085b8ae
anushaarao3 <132684467+anushaarao3@users.noreply.github.com>
2026-06-12 21:04:10 +0530

    Create solution.py

---
+++ longestcommonprefix/anusha/solution.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        for i in range(len(strs[0])):
+            char = strs[0][i]
+
+            for string in strs[1:]:
+                if i == len(string) or string[i] != char:
+                     return strs[0][:i]  
+                       
+        return strs[0]
