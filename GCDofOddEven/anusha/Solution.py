8b7b161b5eded17f8f9ce8b4a6b229be90a80668
anushaarao3 <132684467+anushaarao3@users.noreply.github.com>
2026-07-16 20:50:51 +0530

    Update Solution.py

--- GCDofOddEven/anusha/Solution.py
+++ GCDofOddEven/anusha/Solution.py
@@ -8,15 +8,14 @@ class Solution(object):
         """
         sumEven = 0
         sumOdd = 0
-        
-        # Iterating from 1 to 2*n (inclusive)
+      
         for i in range(1, (n * 2) + 1):
             if i % 2 == 0:
                 sumEven += i
             else:
                 sumOdd += i
                 
-        # Return the Greatest Common Divisor of the two sums
+      
         def gcd(a, b):
             while b != 0:
                 a, b = b, a % b
